a3f2f91b0eccf96caa95da411a0d9b2a640db1cd
Joshua Koo <zz85nus@gmail.com>
2016-08-13 01:09:44 +0800

    First cut at making motion detection

--- video.js
+++ video.js
@@ -17,6 +17,8 @@
  * Complicated CV techniques / object recognizition may not be required at this point.
  */
 
+let ctx, video, canvas, greyscale
+
 
 video = document.querySelector('video')
 // video.currentTime
@@ -37,36 +39,103 @@ video.addEventListener('durationchange', function() {
 	// console.log('durationchange', video.duration);
 });
 
+let processStarted = false;
+
 video.addEventListener('canplaythrough', function() {
 	// let start processing!
 	video.play();
-	canvas = document.createElement('canvas');
-	canvas.width = video.videoWidth; // do resizing?
-	canvas.height = video.videoHeight; //
+	if (!processStarted) {
+		canvas = document.createElement('canvas');
+		width = canvas.width = video.videoWidth / 2 | 0; // do resizing?
+		height = canvas.height = video.videoHeight  / 2 | 0; //
+		greyscale = new Float32Array( width * height );
+		frameBuffer = new Float32Array( width * height ).fill(0);
 
-	console.log(canvas.width, canvas.height);
+		console.log(canvas.width, canvas.height);
 
-	ctx = canvas.getContext('2d');
+		ctx = canvas.getContext('2d');
+		processStarted = true;
+
+		document.body.appendChild(canvas);
+	}
 });
 
 let lastTime = 0;
+
+function greyscaleImage( idata, greyscale ) {
+
+	let data = idata.data;
+	let pixels = idata.width * idata.height;
+
+	// conversion to greyscale
+
+	for ( let i = 0; i < pixels; i++ ) {
+		let ref = i * 4;
+		let r = data[ ref + 0 ] / 255;
+		let g = data[ ref + 1 ] / 255;
+		let b = data[ ref + 2 ] / 255;
+
+		// Y'=0.299R'+0.587G'+0.114B'
+		greyscale[ i ] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+		// greyscale[ i ] = (r + g + b) / 3;
+	}
+
+	return greyscale;
+}
+
+
+
 
 video.addEventListener('timeupdate', function() {
 	const now = video.currentTime;
 
 	if (lastTime !== now) {
-		console.log('time', video.currentTime, now - lastTime);
+		// console.log('time', video.currentTime, now - lastTime);
 		lastTime = now;
 		// video.pause();
 		processFrame();
 	} else {
-		console.log('same!');
+		// console.log('same!');
 	}
 });
 
 function processFrame() {
+
+	// ctx.clearRect(0, 0, width, height);
+	ctx.drawImage( video, 0, 0, width, height );
+	idata = ctx.getImageData( 0, 0, width, height );
+
+
+	greyscaleImage(idata, greyscale);
+
+
+	// do ping pong later
+
+	let summed = 0
+	let pixels = width * height;
+
+	// conversion to greyscale
+
+	for ( let i = 0; i < pixels; i++ ) {
+		const diff = greyscale[ i ] - frameBuffer[ i ];
+		summed += diff * diff;
+	}
+
+	for ( let i = 0; i < pixels; i++ ) {
+		frameBuffer[i] = greyscale[ i ];
+	}
+
+	// frameBuffer.set(greyscale);
+
+	const score = summed / pixels;
+	if (score > 0.0005) // 0.001
+	console.log('diff', video.currentTime, score);
+
+
 	// video.play();
-	let seek = 1;
+	let seek = 1 / 29.97;
+	// 0.25 1/24 1/29.97 30
+
 	if (video.currentTime + seek > video.duration) {
 		return;
 	}
